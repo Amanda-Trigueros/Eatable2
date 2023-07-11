12da6e2b7f38820b26af5e257980ebd4f14e1f7d
Amanda Trigueros <amandatrigueros01@gmail.com>
2023-07-11 16:34:38 -0600

    chore: add styles to input component

---
+++ src/components/Input/styles.jsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+import { colors } from "../../styles/colors";
+
+export const StyledInput = styled("input")`
+  background-color: ${colors.pallette.lightGray};
+  border: none;
+  border-bottom: 1px solid ${colors.pallette.black};
+  display: block;
+  padding: 0.25rem 0;
+  font-size: 1.125rem;
+  width: 100%;
+  ::placeholder {
+    opacity: 0.5;
+  }
+`;
+
+export const StyledLabel = styled.label`
+  color: ${colors.pallette.gray};
+  font-weight: 600;
+  font-size: 0.875rem;
+`;
