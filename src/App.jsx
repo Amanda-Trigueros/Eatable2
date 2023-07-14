2fdd6e5b276cf726d11e582b75679f7009748a7d
Amanda Trigueros <amandatrigueros01@gmail.com>
2023-07-13 20:11:13 -0600

    chore: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { Routes, Route, Navigate } from "react-router";
 import Products from "./pages/products-page";
 import SingleProduct from "./pages/single-product-page";
 import CreateProduct from "./pages/create-page";
+import DeleteProduct from "./pages/delete-page";
 
 function App() {
-  return <CreateProduct />;
-}
+  return (
+    
+    <Routes>
+    <Route path="/">
+        <Route index element={<Navigate to="/products" />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProduct />} />
+        <Route path="create" element={<CreateProduct />} />
+      </Route>
+    </Routes>
+    )
+  }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { Global } from "@emotion/react";
 import { reset, global } from "./styles/global.js";
 
@@ -9,6 +10,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Global styles={reset} />
     <Global styles={global} />
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
